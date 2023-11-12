f11fa2c32f67bf64145691193319071089aa8bf4
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-11-12 01:33:16 +0100

    add rating

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,7 @@
   };
   const classNames = {
     favorite: 'favorite',
+    hidden: 'hidden',
   };
   //reference to template
   const templates = {
@@ -31,20 +32,36 @@
 
   function render() {
     for (const book of dataSource.books) {
+      const ratingBgc = determineRatingBgc(book.rating);
+      const ratingWidth = determineRatingWidth(book.rating);
       //generuje na podstawie klasy#
-      const generateHTML = templates.bookList(book);
+      const generateHTML = templates.bookList({
+        ...book,
+        ratingBgc: ratingBgc,
+        ratingWidth: ratingWidth,
+      });
       // generuje na podsatawie function.js
       const generatedDOM = utils.createDOMFromHTML(generateHTML);
 
       // Wygenerowany element DOM dołącz jako nowe dziecko DOM do listy .books-list.
       booksListContainer.appendChild(generatedDOM);
+
+      /*const tamplateRatting = document.querySelector(
+        'style="width: {{' +
+          ratingWidth +
+          '}}%; background: {{' +
+          ratingBgc +
+          '}}"'
+      );*/
     }
   }
+
   let arrFavorites = [];
   let arrfilters = [];
   //referencj do filtra
   const filtersListContainer = document.querySelector(select.addAction.filters);
   console.log(filtersListContainer);
+
   function initActions() {
     //referencja do listy
     const favoriteTriggers = document.querySelectorAll(select.addAction.heart);
@@ -94,12 +111,51 @@
         } else {
           arrfilters.push(filterValue);
           event.target.checked = true;
+          filterBooks();
         }
 
         console.log('click', arrfilters);
       }
       //event.preventDefault();
     });
+  }
+  function filterBooks() {
+    for (const filterBook of dataSource.books) {
+      let shouldBeHidden = false;
+      for (const filter of arrfilters) {
+        if (!filterBook.details[filter]) {
+          shouldBeHidden = true;
+          //break;
+        }
+      }
+      const bookHidden = document.querySelector(
+        '.book__image[data-id="' + filterBook.id + '"]'
+      );
+      if (shouldBeHidden) {
+        bookHidden.classList.add(classNames.hidden);
+        console.log('hidden', bookHidden);
+      } else {
+        bookHidden.classList.remove(classNames.hidden);
+        console.log('hidden', bookHidden);
+      }
+    }
+  }
+
+  function determineRatingBgc(rating) {
+    if (rating < 6) {
+      return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if (rating > 9) {
+      return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+  }
+  function determineRatingWidth(rating) {
+    const width = rating * 10;
+    const limitedWidth = Math.min(Math.max(width, 0), 100);
+    return limitedWidth;
   }
 
   render();
